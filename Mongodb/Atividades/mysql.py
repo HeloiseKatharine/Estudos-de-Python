6631b2debeab3703b3a0bf24b40ab274dcb59029
Heloise Katharine <katharineheloise@gmail.com>
2021-09-01 16:06:30 -0300

    mysql

--- Mongodb/Atividades/mysql.py
+++ Mongodb/Atividades/mysql.py
@@ -1,5 +1,28 @@
 import MySQLdb
 
+def get_database():
+    from pymongo import MongoClient
+    import pymongo
+
+    # Forneça o url do atlas mongodb para conectar python a mongodb usando pymongo
+    CONNECTION_STRING = "#"
+
+    # Crie uma conexão usando MongoClient. Você pode importar MongoClient ou usar pymongo.MongoClient
+    from pymongo import MongoClient
+    client = MongoClient(CONNECTION_STRING)
+
+    # Cria o banco de dados.
+    
+    return client['soulcodeTeste2']
+  
+
+def mostrarDocumentos():
+    dbname = get_database()
+    collection_name = dbname["itens_mysql_export2"]
+    detalhes_itens = collection_name.find()
+    for item in detalhes_itens:
+        print(item)
+
 db = MySQLdb.connect(host="185.201.11.44", user="u781216269_rootx", passwd="h5S3J#oP1@", db = "u781216269_library")
 
 cursor = db.cursor()
@@ -18,17 +41,24 @@ for linha in linhas:
     print("ISBN: ", linha[4])
     print("Preço R$0: ", linha[5])
 
-'''
 dbname = get_database()
 for linha in linhas:
-    Collection_name = dbname["itens_mysql_export2"]
-    item = {
-        "_id":linha[0],
-        "nome":linha[1],
-        "ISBN":linha[4],
-        "PREÇO":linha[5]
-    }
-    Collection_name.insert_one(item)
-print("Documento vindo do MySQL inserido em Mongo!")
+   collection_name = dbname["itens_mysql_export2"]  
+   item = {
+      "_id": linha[0], 
+      "dado_1": linha[1],
+      "ISBN": linha[4],
+      "Preço": linha[5]
+   }
+   collection_name.insert_one(item)
+print("Documento vindo do MySQL inserido em Mongo!")   
 
-'''
+mostrarDocumentos()
+
+
+# admin 6
+# tblauthors 4
+# tblcategory 5
+# tblissuedbookdetails 7
+# tblstudents 9
+
